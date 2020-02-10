ab870f6921ea11cacfa1e58f8ef7ed3efec35d75
Alex Barros <dossantosab@gmail.com>
2020-02-09 22:33:04 -0300

    updating simulations

--- Ensemble Learning/ensemble_challenge.py
+++ Ensemble Learning/ensemble_challenge.py
@@ -21,16 +21,17 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from keras.utils import np_utils
 from sklearn import datasets, linear_model
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score,cross_validate
 from sklearn import metrics
 from sklearn.datasets import make_moons
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score,f1_score, recall_score,precision_score,roc_auc_score
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier,AdaBoostClassifier,GradientBoostingClassifier
 from sklearn.preprocessing import LabelEncoder
 import time as time
-
+from sklearn.metrics import make_scorer
+from sklearn.svm import LinearSVC
 
 # Step1: Create data set
 
@@ -54,7 +55,6 @@ classe_dummy = np_utils.to_categorical(classe_encoder)
 
 X_train, X_test, y_train, y_test = train_test_split(X, classe_encoder, test_size=0.30)
 
-scoring = ['accuracy', 'precision','f1','recall','roc_auc']
 
 #Linear Regression
 #lm = linear_model.LinearRegression()
@@ -69,28 +69,25 @@ clf = DecisionTreeClassifier()
 resultados = cross_val_score(estimator = clf,
                              X = X, y = classe_encoder,
                              cv = 10, scoring = 'accuracy')
+
 media = resultados.mean()
 desvio = resultados.std()
-#getting time
+#getting metrics
 
 start = time.time()
 clf = DecisionTreeClassifier()
 clf.fit(X_train, y_train)
 end = time.time()
 y_pred = clf.predict(X_test)
-accuracy_score(y_test, y_pred)
-tempo =[['Decision Tree',(end-start),media, desvio]]
+acc=accuracy_score(y_test, y_pred)
+f1=f1_score(y_test, y_pred, average='macro')
+recall=recall_score(y_test, y_pred, average='macro')
+precision=precision_score(y_test, y_pred, average='macro')
+roc=roc_auc_score(y_test, y_pred,multi_class='ovr')
+metrics =[['Decision Tree',(end-start),media, desvio,acc,f1,recall,precision]]
 
-#Usando Cross Validation
-
-
-
-
-
-
-
-
-# Step 4: Fit a Random Forest model, " compared to "Decision Tree model, accuracy go up by 5%
+# Step 3: Fit a Random Forest Model model as comparison
+#Using cross-validation
 clf = RandomForestClassifier(n_estimators=100, max_features="auto",random_state=0)
 resultados = cross_val_score(estimator = clf,
                              X = X, y = classe_encoder,
@@ -98,28 +95,25 @@ resultados = cross_val_score(estimator = clf,
 media = resultados.mean()
 desvio = resultados.std()
 
-
+#getting metrics
 start = time.time()
 clf = RandomForestClassifier(n_estimators=100, max_features="auto",random_state=0)
 clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
-accuracy_score(y_test, y_pred)
 end = time.time()
-tempo.append(['Random Forest',(end-start)])
-
-
-
-
-
-
-# Step 5: Fit a AdaBoost model, " compared to "Decision Tree model, accuracy go up by 10%
-clf = AdaBoostClassifier(n_estimators=100)
-clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
-accuracy_score(y_test, y_pred)
+acc=accuracy_score(y_test, y_pred)
+f1=f1_score(y_test, y_pred, average='macro')
+recall=recall_score(y_test, y_pred, average='macro')
+precision=precision_score(y_test, y_pred, average='macro')
+roc=roc_auc_score(y_test, y_pred,multi_class='ovr')
+metrics.append(['Random Forest',(end-start),media, desvio,acc,f1,recall,precision,roc])
 
 
 
+
+
+
+# Step 4: Fit a AdaBoost model,
 clf = AdaBoostClassifier(n_estimators=100)
 resultados = cross_val_score(estimator = clf,
                              X = X, y = classe_encoder,
@@ -127,45 +121,64 @@ resultados = cross_val_score(estimator = clf,
 media = resultados.mean()
 desvio = resultados.std()
 
-
-
-
-
-# Step 6: Fit a Gradient Boosting model, " compared to "Decision Tree model, accuracy go up by 10%
-clf = GradientBoostingClassifier(n_estimators=100)
+#getting metrics
+start = time.time()
+clf = AdaBoostClassifier(n_estimators=100)
 clf.fit(X_train, y_train)
+end = time.time()
 y_pred = clf.predict(X_test)
-accuracy_score(y_test, y_pred)
+acc=accuracy_score(y_test, y_pred)
+f1=f1_score(y_test, y_pred, average='macro')
+recall=recall_score(y_test, y_pred, average='macro')
+precision=precision_score(y_test, y_pred, average='macro')
+roc=roc_auc_score(y_test, y_pred,multi_class='ovr')
+metrics.append(['AdaBoost',(end-start),media, desvio,acc,f1,recall,precision,roc])
 
 
-
+# Step 5: Fit a Gradient Boosting model, " compared to "Decision Tree model, accuracy go up by 10%
 clf = GradientBoostingClassifier(n_estimators=100)
 resultados = cross_val_score(estimator = clf,
                              X = X, y = classe_encoder,
                              cv = 10, scoring = 'accuracy')
 media = resultados.mean()
 desvio = resultados.std()
+
+#getting metrics
+start = time.time()
+clf = GradientBoostingClassifier(n_estimators=100)
+clf.fit(X_train, y_train)
+end = time.time()
+y_pred = clf.predict(X_test)
+acc=accuracy_score(y_test, y_pred)
+f1=f1_score(y_test, y_pred, average='macro')
+recall=recall_score(y_test, y_pred, average='macro')
+precision=precision_score(y_test, y_pred, average='macro')
+roc=roc_auc_score(y_test, y_pred,multi_class='ovr')
+metrics.append(['GradientBoosting',(end-start),media, desvio,acc,f1,recall,precision,roc])
 
 
 #SVM
-
-from sklearn.svm import LinearSVC
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
-
-start = time.time()
-clf = LinearSVC(random_state=0, tol=1e-5, dual=False,)
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
-accuracy_score(y_test, y_pred)
-end = time.time()
-print("Single SVC", end - start, clf.score(X,y))
-
-
-start = time.time()
+# Step 6: Fit a Linear SVM
 clf = LinearSVC(random_state=0, tol=1e-5, dual=False,)
 resultados = cross_val_score(estimator = clf,
                              X = X, y = classe_encoder,
                              cv = 10, scoring = 'accuracy')
 media = resultados.mean()
 desvio = resultados.std()
+
+#getting metrics
+start = time.time()
+clf = LinearSVC(random_state=0, tol=1e-5, dual=False,)
+clf.fit(X_train, y_train)
+end = time.time()
+y_pred = clf.predict(X_test)
+acc=accuracy_score(y_test, y_pred)
+f1=f1_score(y_test, y_pred, average='macro')
+recall=recall_score(y_test, y_pred, average='macro')
+precision=precision_score(y_test, y_pred, average='macro')
+roc=roc_auc_score(y_test, y_pred,multi_class='ovr')
+metrics.append(['SVM',(end-start),media, desvio,acc,f1,recall,precision,roc])
+
+
+# Step 7: Fit a Neural Network
+
